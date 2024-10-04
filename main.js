1dfa01da88d4163fff88d71f0bd8257fd4feadc8
valort0n <beloshnez98@bk.ru>
2024-10-04 09:19:16 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+export const pi = 3.14;
+export const e = 2.718;
+export const square = (x) => {
+    return x * x
+};
+
+export const surfaceArea = (r) => {
+    return 4 * pi * square(r);
+};
---
+++ planet.js
@@ -0,0 +1,10 @@
+import {surfaceArea , square} from './main.js';
+
+
+const sufceOfMars = surfaceArea(3390);
+const surfOfUpiter = surfaceArea(2230);
+const yearsSquared = square(2017);
+console.log(`Радиус марса :${sufceOfMars}, 
+Радиус Юпитера :${surfOfUpiter}, 
+Хуйня${yearsSquared}`);
+
